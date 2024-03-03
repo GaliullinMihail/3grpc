086115d38618001a2e11b08199df5a368bf34a8b
AndrejHoryakov <andrejhorakov47232@gmail.com>
2024-03-03 23:25:28 +0300

    add some files

---
+++ 3/GrpcStreamingChat/chat-client/components/ChatBubble.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import "./style.css";
+// import { StreamMessage } from "../proto/random_pb";
+
+const ChatBubble = ({ message, isCurrentUser }) => {
+  const { senderName, message: text } = message;
+  return (
+    <Grid
+      container
+      item
+      style={{
+        width: "100%",
+        justifyContent: isCurrentUser ? "flex-end" : "flex-start",
+      }}
+    >
+      <blockquote
+        className="speech-bubble"
+        style={{ backgroundColor: isCurrentUser ? "#bad4ff" : undefined }}
+      >
+        <p>{text}</p>
+        {!isCurrentUser && <cite>{senderName}</cite>}
+      </blockquote>
+    </Grid>
+  );
+};
+
+export default ChatBubble;
